128520736270740c85157eb809aa8bdc0f992221
kisliakovsky <kisliakovskiii@yandex.ru>
2018-05-24 16:25:02 +0300

    project dirs

---
+++ src/main/java/ru/devsand/classfinder/Main.java
@@ -0,0 +1,9 @@
+package ru.devsand.classfinder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/ru/devsand/classfinder/MainTest.java
@@ -0,0 +1,9 @@
+package ru.devsand.classfinder;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+
+}
